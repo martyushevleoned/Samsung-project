e052a37fb8a5267ed77ca1aeb802265a6688d313
martyushevleoned <Lyonya.martyushev@mail.ru>
2020-04-26 12:29:32 +0300

    optimize

--- Flappy2/app/src/main/java/com/example/flappy2/GameView.java
+++ Flappy2/app/src/main/java/com/example/flappy2/GameView.java
@@ -72,21 +72,9 @@ public class GameView extends View {
         load();
         MainActivity.loadData(cont);
 
+
         Timer t = new Timer();
         t.start();
-    }
-
-    private void load() {
-        swooshing = MediaPlayer.create(cont, R.raw.sfx_swooshing);
-        die = MediaPlayer.create(cont, R.raw.sfx_die);
-        hit = MediaPlayer.create(cont, R.raw.sfx_hit);
-
-        for (int i = 0; i < points.length; i++) {
-            points[i] = MediaPlayer.create(cont, R.raw.sfx_point);
-        }
-        for (int i = 0; i < wings.length; i++) {
-            wings[i] = MediaPlayer.create(cont, R.raw.sfx_wing);
-        }
     }
 
     @Override
@@ -105,84 +93,19 @@ public class GameView extends View {
             canvas.drawColor(Color.WHITE);
     }
 
-    protected void drawScore(Canvas canvas, int score) {
-        Paint p = new Paint();
-        p.setSubpixelText(true);
-        p.setColor(Color.WHITE);
-        p.setTextSize(150);
-
-        if (stage < 3) {
-            if (score < 10)
-                canvas.drawText("" + score, (float) (getWidth() / 2) - 50, 300, p);
-            else if (score < 100)
-                canvas.drawText("" + score, (float) (getWidth() / 2) - 100, 300, p);
-            else
-                canvas.drawText("" + score, (float) (getWidth() / 2) - 150, 300, p);
-        }
-
-    }
-
-    protected void drawStart(Canvas canvas) {
-        Paint p = new Paint();
-
-        if (stage == 0) {
-            Bitmap tap = BitmapFactory.decodeResource(getResources(), R.drawable.taptostart);
-            canvas.drawBitmap(tap, (float) (getWidth() - tap.getWidth()) / 2, (float) getHeight() / 2 + 100, p);
-
-            Bitmap ready = BitmapFactory.decodeResource(getResources(), R.drawable.getready);
-            canvas.drawBitmap(ready, (float) (getWidth() - ready.getWidth()) / 2, (float) getHeight() / 2 - 100 - ready.getHeight(), p);
-        }
-    }
-
-    protected void drawFon(Canvas canvas) {
-        Paint p = new Paint();
-        canvas.drawBitmap(fon, (float) (getWidth() - fon.getWidth()) / 2, (float) (getHeight() - fon.getHeight()) / 2, p);
-    }
-
-    protected void drawGround(Canvas canvas) {
-        Paint p = new Paint();
-        canvas.drawBitmap(ground, groundX, getHeight() - groundHeight, p);
-        canvas.drawBitmap(ground, groundX + ground.getWidth(), getHeight() - groundHeight, p);
-        if (stage < 2)
-            if (stage != 0)
-                groundX += groundVX;
-        if (-groundX > ground.getWidth()) groundX += ground.getWidth();
-
-    }
-
-    protected void drawResults(Canvas canvas) {
-        if (stage >= 3) {
-            Paint p = new Paint();
-
-            p.setTextSize(90);
-            p.setColor(Color.WHITE);
-
-            if (resultEnd <= getHeight() / 2 - gameOver.getHeight() - 100) {
-                resultEnd += 15;
-            }
-            canvas.drawBitmap(gameOver, (float) (getWidth() - gameOver.getWidth()) / 2, resultEnd, p);
-
-            if (resultY >= getHeight() / 2 + 100) {
-                resultY -= 15;
-            } else {
-                stage = 4;
-            }
-            canvas.drawBitmap(results, (float) (getWidth() - results.getWidth()) / 2, resultY, p);
-            canvas.drawBitmap(medal,
-                    (float) (((getWidth() - results.getWidth()) / 2) + (results.getWidth() / 3) - medal.getWidth()),
-                    (float) (resultY + ((results.getHeight() - medal.getHeight()) / 2)), p);
-
-            canvas.drawText("" + score, (float) (((getWidth() - results.getWidth()) / 2) + (results.getWidth() * 2 / 3)), (float) (resultY + results.getHeight() * 6 / 16), p);
-            canvas.drawText("" + maxScore, (float) (((getWidth() - results.getWidth()) / 2) + (results.getWidth() * 2 / 3)), (float) (resultY + results.getHeight() * 13 / 16), p);
-
-        }
-    }
-
     protected void update() {
 
-        if (stage > 0 && stage < 3) {
-            flappy.update(timerInterval, stage);
+        if (stage == 1 || stage == 2) {
 
+            //-----------------------------------UPDATE-----------------------------------
+            flappy.update(timerInterval, stage);
+            if (stage == 0 || stage == 1)
+                if (stage != 0) {
+                    tube1.update(tubeSpawn, getHeight());
+                    tube2.update(tubeSpawn, getHeight());
+                }
+
+            //-----------------------------------POINT-----------------------------------
             if (flappy.getX() + error > tube1.getEdge()) {
                 if (!point) {
                     point = true;
@@ -198,13 +121,7 @@ public class GameView extends View {
                 }
             }
 
-
-            if (stage == 0 || stage == 1)
-                if (stage != 0) {
-                    tube1.update(tubeSpawn, getHeight());
-                    tube2.update(tubeSpawn, getHeight());
-                }
-
+            //-----------------------------------HIT-----------------------------------
             if (flappy.getY() + flappy.getFrameHeight() > getHeight() - groundHeight) {
                 flappy.setY(getHeight() - flappy.getFrameHeight() - groundHeight);
                 stage = 3;
@@ -224,38 +141,40 @@ public class GameView extends View {
                 if (MainActivity.saveThis < maxScore) {
                     MainActivity.saveThis = maxScore;
                     MainActivity.saveData(cont);
-                }else {
+                } else {
                     maxScore = MainActivity.saveThis;
                 }
             }
 
+            //-----------------------------------CHECK-----------------------------------
             if (stage == 1) {
                 if (lose(flappy.getX() + error, flappy.getY())) {
                     stage = 2;
-                    flappy.setVy(flappy.getVy() + 10);
+                    flappy.setVy(flappy.getVy() + 5);
                     soundPlay(die);
                 }
                 if (stage != 2)
                     if (lose(flappy.getX() + flappy.getFrameWidth() - error, flappy.getY())) {
                         stage = 2;
-                        flappy.setVy(flappy.getVy() + 10);
+                        flappy.setVy(flappy.getVy() + 5);
                         soundPlay(die);
                     }
                 if (stage != 2)
                     if (lose(flappy.getX() + error, flappy.getY() + flappy.getFrameHeight())) {
                         stage = 2;
-                        flappy.setVy(flappy.getVy() + 10);
+                        flappy.setVy(flappy.getVy() + 5);
                         soundPlay(die);
                     }
                 if (stage != 2)
                     if (lose(flappy.getX() + flappy.getFrameWidth() - error, flappy.getY() + flappy.getFrameHeight())) {
                         stage = 2;
-                        flappy.setVy(flappy.getVy() + 10);
+                        flappy.setVy(flappy.getVy() + 5);
                         soundPlay(die);
                     }
             }
         }
 
+        //-----------------------------------SKIP-FIRST-FRAME-----------------------------------
         if (stage < -1) {
             stage++;
         }
@@ -264,14 +183,66 @@ public class GameView extends View {
             tubeSpawn = getWidth() + 300;
             restart();
         }
+
+
         invalidate();
     }
 
-    protected boolean lose(double x, double y) {
-        if ((x > tube1.getX() && x < tube1.getEdge()) && (y < tube1.getUpTube() || y > tube1.getDownTube()))
-            return true;
-        else
-            return (x > tube2.getX() && x < tube2.getEdge()) && (y < tube2.getUpTube() || y > tube2.getDownTube());
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int eventAction = event.getAction();
+        if (eventAction == MotionEvent.ACTION_DOWN) {
+
+            //-----------------------------------FLY-----------------------------------
+            if (stage == 0 || stage == 1) {
+                flappy.setVy(-25);
+                stage = 1;
+                soundPlay(wings[wingCounter]);
+                wingCounter++;
+                wingCounter %= wings.length;
+            }
+
+            //-----------------------------------RESTART-----------------------------------
+            if (stage == 4) {
+                soundPlay(swooshing);
+                restart();
+            }
+        }
+        return true;
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    class Timer extends CountDownTimer {
+        Timer() {
+            super(Integer.MAX_VALUE, timerInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            update();
+        }
+
+        @Override
+        public void onFinish() {
+        }
+    }
+
+    private void load() {
+        swooshing = MediaPlayer.create(cont, R.raw.sfx_swooshing);
+        die = MediaPlayer.create(cont, R.raw.sfx_die);
+        hit = MediaPlayer.create(cont, R.raw.sfx_hit);
+
+        for (int i = 0; i < points.length; i++)
+            points[i] = MediaPlayer.create(cont, R.raw.sfx_point);
+
+        for (int i = 0; i < wings.length; i++)
+            wings[i] = MediaPlayer.create(cont, R.raw.sfx_wing);
+    }
+
+    protected void soundPlay(MediaPlayer s) {
+        s.start();
     }
 
     protected void restart() {
@@ -327,44 +298,83 @@ public class GameView extends View {
         score = 0;
     }
 
-    protected void soundPlay(MediaPlayer s) {
-        s.start();
+    protected void drawResults(Canvas canvas) {
+        if (stage == 3 || stage == 4) {
+            Paint p = new Paint();
+
+            p.setTextSize(90);
+            p.setColor(Color.WHITE);
+
+            if (resultEnd <= getHeight() / 2 - gameOver.getHeight() - 100) {
+                resultEnd += 10;
+            }
+            canvas.drawBitmap(gameOver, (float) (getWidth() - gameOver.getWidth()) / 2, resultEnd, p);
+
+            if (resultY >= getHeight() / 2 + 100) {
+                resultY -= 10;
+            } else {
+                stage = 4;
+            }
+            canvas.drawBitmap(results, (float) (getWidth() - results.getWidth()) / 2, resultY, p);
+            canvas.drawBitmap(medal,
+                    (float) (((getWidth() - results.getWidth()) / 2) + (results.getWidth() / 3) - medal.getWidth()),
+                    (float) (resultY + ((results.getHeight() - medal.getHeight()) / 2)), p);
+
+            canvas.drawText("" + score, (float) (((getWidth() - results.getWidth()) / 2) + (results.getWidth() * 2 / 3)), (float) (resultY + results.getHeight() * 6 / 16), p);
+            canvas.drawText("" + maxScore, (float) (((getWidth() - results.getWidth()) / 2) + (results.getWidth() * 2 / 3)), (float) (resultY + results.getHeight() * 13 / 16), p);
+
+        }
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        int eventAction = event.getAction();
-        if (eventAction == MotionEvent.ACTION_DOWN) {
-            if (stage == 0 || stage == 1) {
-                flappy.setVy(-25);
-                stage = 1;
-                soundPlay(wings[wingCounter]);
-                wingCounter++;
-                wingCounter %= wings.length;
-            }
-
-            if (stage == 4) {
-                soundPlay(swooshing);
-                restart();
-            }
-        }
-        return true;
+    protected boolean lose(double x, double y) {
+        if ((x > tube1.getX() && x < tube1.getEdge()) && (y < tube1.getUpTube() || y > tube1.getDownTube()))
+            return true;
+        else
+            return (x > tube2.getX() && x < tube2.getEdge()) && (y < tube2.getUpTube() || y > tube2.getDownTube());
     }
 
-    class Timer extends CountDownTimer {
-        Timer() {
-            super(Integer.MAX_VALUE, timerInterval);
+    protected void drawGround(Canvas canvas) {
+        Paint p = new Paint();
+        canvas.drawBitmap(ground, groundX, getHeight() - groundHeight, p);
+        canvas.drawBitmap(ground, groundX + ground.getWidth(), getHeight() - groundHeight, p);
+        if (stage < 2)
+            if (stage != 0)
+                groundX += groundVX;
+        if (-groundX > ground.getWidth()) groundX += ground.getWidth();
+
+    }
+
+    protected void drawScore(Canvas canvas, int score) {
+        Paint p = new Paint();
+        p.setSubpixelText(true);
+        p.setColor(Color.WHITE);
+        p.setTextSize(150);
+
+        if (stage == 0 || stage == 1 || stage == 2) {
+            if (score < 10)
+                canvas.drawText("" + score, (float) (getWidth() / 2) - 50, 300, p);
+            else if (score < 100)
+                canvas.drawText("" + score, (float) (getWidth() / 2) - 100, 300, p);
+            else
+                canvas.drawText("" + score, (float) (getWidth() / 2) - 150, 300, p);
         }
 
-        @Override
-        public void onTick(long millisUntilFinished) {
-            update();
-        }
+    }
 
-        @Override
-        public void onFinish() {
+    protected void drawStart(Canvas canvas) {
+        Paint p = new Paint();
+
+        if (stage == 0) {
+            Bitmap tap = BitmapFactory.decodeResource(getResources(), R.drawable.taptostart);
+            canvas.drawBitmap(tap, (float) (getWidth() - tap.getWidth()) / 2, (float) getHeight() / 2 + 100, p);
+
+            Bitmap ready = BitmapFactory.decodeResource(getResources(), R.drawable.getready);
+            canvas.drawBitmap(ready, (float) (getWidth() - ready.getWidth()) / 2, (float) getHeight() / 2 - 100 - ready.getHeight(), p);
         }
+    }
+
+    protected void drawFon(Canvas canvas) {
+        Paint p = new Paint();
+        canvas.drawBitmap(fon, (float) (getWidth() - fon.getWidth()) / 2, (float) (getHeight() - fon.getHeight()) / 2, p);
     }
 }
