6eef703d22f29bd62658084bb29eeb9d70081195
martyushevleoned <Lyonya.martyushev@mail.ru>
2020-05-16 23:40:25 +0300

    tetris

---
+++ Tetris/app/src/main/java/com/example/tetris/GameView.java
@@ -0,0 +1,263 @@
+package com.example.tetris;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.CountDownTimer;
+import android.view.View;
+
+@SuppressLint("ViewConstructor")
+public class GameView extends View {
+
+    Context cont;
+
+    Paint p = new Paint();
+
+    private int stage = -10;
+    private int counter = 0;
+    public static int timerInterval = 50;
+    public static int h;
+    public static int w = 10;
+    public static int mainBlockX;
+    public static int mainBlockY = -1;
+    public static int direction = 0;
+    public static int shapeNum = 0;
+    public static int[][] area;
+    public static int[][][][] shape = {
+            {
+                    {{0, 1, -1, 0}, {0, 0, 0, -1}},
+                    {{0, 0, 0, 1}, {0, 1, -1, 0}},
+                    {{0, 1, -1, 0}, {0, 0, 0, 1}},
+                    {{0, 0, 0, -1}, {0, 1, -1, 0}}
+            },
+            {
+                    {{0, 0, 0, 1}, {0, -1, 1, -1}},
+                    {{0, -1, 1, 1}, {0, 0, 0, 1}},
+                    {{0, 0, 0, -1}, {0, -1, 1, 1}},
+                    {{0, -1, 1, -1}, {0, 0, 0, -1}}
+            },
+            {
+                    {{0, 0, 0, -1}, {0, -1, 1, -1}},
+                    {{0, 1, -1, 1}, {0, 0, 0, -1}},
+                    {{0, 0, 0, 1}, {0, -1, 1, 1}},
+                    {{0, 1, -1, -1}, {0, 0, 0, 1}}
+            },
+            {
+                    {{0, 0, 1, -1}, {0, -1, -1, 0}},
+                    {{0, -1, -1, 0}, {0, 0, -1, 1}}
+            },
+            {
+                    {{0, 0, -1, 1}, {0, -1, -1, 0}},
+                    {{0, 1, 1, 0}, {0, 0, -1, 1}}
+            },
+            {
+                    {{0, 0, 0, 0}, {0, -1, 1, 2}},
+                    {{0, -1, 1, 2}, {0, 0, 0, 0}}
+            },
+            {
+                    {{0, 1, 0, 1}, {0, 1, 1, 0}}
+            }
+    };
+
+    public GameView(Context context) {
+        super(context);
+        cont = context;
+        p.setAntiAlias(true);
+
+        Timer t = new Timer();
+        t.start();
+    }
+
+    class Timer extends CountDownTimer {
+        Timer() {
+            super(Integer.MAX_VALUE, timerInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            update();
+        }
+
+        @Override
+        public void onFinish() {
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (stage >= 0) {
+
+            drawMatrix(canvas);
+
+            for (int i = 0; i < shape[shapeNum][direction][0].length; i++) {
+                p.setColor(Color.GREEN);
+                drawPix(canvas, mainBlockX + shape[shapeNum][direction][0][i], mainBlockY + shape[shapeNum][direction][1][i]);
+            }
+            p.setColor(Color.RED);
+            drawPix(canvas, mainBlockX, mainBlockY);
+
+            drawTable(canvas);
+
+        } else {
+            canvas.drawARGB(255, 0, 0, 0);
+        }
+    }
+
+    protected void update() {
+
+        if (stage < -1) {
+            stage++;
+            if (stage == -1) {
+
+                h = getHeight();
+                h /= ((double) getWidth() / w);
+
+                area = new int[w][h];
+
+                mainBlockX = w / 2 - 1;
+                stage = 0;
+            }
+        } else {
+
+            if (counter == 0) {
+
+                stopCheck();
+
+            }
+            counter++;
+            counter %= 6;
+
+
+        }
+        invalidate();
+    }
+
+    protected void deleteLayer() {
+        boolean t;
+
+        for (int i1 = 0; i1 < h; i1++) {
+
+            t = true;
+
+            for (int i = 0; i < w; i++) {
+                if (area[i][i1] == 0) {
+                    t = false;
+                    break;
+                }
+            }
+
+            if (t) {
+                for (int j1 = i1; j1 > 0; j1--) {
+                    for (int j = 0; j < w; j++) {
+                        area[j][j1] = area[j][j1 - 1];
+                    }
+                }
+            }
+        }
+    }
+
+    protected void stopCheck() {
+
+        boolean t = false;
+
+        for (int i = 0; i < shape[shapeNum][direction][0].length; i++) {
+            if (mainBlockY + shape[shapeNum][direction][1][i] >= h - 1) {
+                t = true;
+                break;
+            }
+            if (mainBlockY + shape[shapeNum][direction][1][i] + 1 < h && mainBlockY + shape[shapeNum][direction][1][i] + 1 >= 0)
+                if (area[mainBlockX + shape[shapeNum][direction][0][i]][mainBlockY + shape[shapeNum][direction][1][i] + 1] != 0) {
+                    t = true;
+                    break;
+                }
+        }
+
+        if (t) {
+            for (int i = 0; i < shape[shapeNum][direction][0].length; i++) {
+                if (mainBlockY + shape[shapeNum][direction][1][i] < h)
+                    if (mainBlockX + shape[shapeNum][direction][0][i] < h && mainBlockY + shape[shapeNum][direction][1][i] >= 0)
+                        area[mainBlockX + shape[shapeNum][direction][0][i]][mainBlockY + shape[shapeNum][direction][1][i]] = 1;
+            }
+
+            deleteLayer();
+
+            newShape();
+        } else
+            mainBlockY++;
+    }
+
+    protected void newShape() {
+        boolean t = false;
+        for (int[] ints : area) {
+            if (ints[0] != 0) {
+                t = true;
+                break;
+            }
+        }
+
+        if (t) {
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    area[i][j] = 0;
+                }
+            }
+        }
+
+        timerInterval = 50;
+        shapeNum++;
+        shapeNum %= shape.length;
+        direction = 0;
+        mainBlockY = 0;
+        mainBlockX = w / 2 - 1;
+    }
+
+    protected void drawPix(Canvas canvas, int x, int y) {
+        if (y < h && y >= 0)
+            canvas.drawRect((float) getWidth() * x / w, (float) getHeight() * y / h, (float) getWidth() * (x + 1) / w, (float) getHeight() * (y + 1) / h, p);
+    }
+
+    protected void drawMatrix(Canvas canvas) {
+
+        p.setColor(Color.BLUE);
+
+        for (int i = 0; i < area.length; i++) {
+            for (int j = 0; j < area[i].length; j++) {
+                if (area[i][j] != 0)
+                    drawPix(canvas, i, j);
+            }
+        }
+    }
+
+    protected void drawTable(Canvas canvas) {
+        p.setColor(Color.BLACK);
+        for (int i = 0; i <= w; i++) {
+            canvas.drawLine((float) getWidth() * i / w, 0, (float) getWidth() * i / w, getHeight(), p);
+        }
+        for (int i = 0; i <= h; i++) {
+            canvas.drawLine(0, (float) getHeight() * i / h, getWidth(), (float) getHeight() * i / h, p);
+        }
+    }
+
+    public static boolean rightCheck(int x, int y) {
+
+        if (x < w - 1)
+            if (x == w - 2)
+                return true;
+            else return area[x + 1][y] == 0;
+        else
+            return false;
+    }
+
+    public static boolean leftCheck(int x, int y) {
+        if (x > 0)
+            if (x == 1)
+                return true;
+            else return area[x - 1][y] == 0;
+        else
+            return false;
+    }
+}
---
+++ Tetris/app/src/main/java/com/example/tetris/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.tetris;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
+
+import static com.example.tetris.GameView.direction;
+import static com.example.tetris.GameView.mainBlockX;
+import static com.example.tetris.GameView.mainBlockY;
+import static com.example.tetris.GameView.shape;
+import static com.example.tetris.GameView.shapeNum;
+import static com.example.tetris.GameView.timerInterval;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    public GestureDetectorCompat gd;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+        setContentView(R.layout.activity_main);
+        ((FrameLayout) findViewById(R.id.idFlame)).addView(new GameView(this));
+
+        gd = new GestureDetectorCompat(this, this);
+        gd.setOnDoubleTapListener(this);
+    }
+
+    static int maxScore = 0;
+    static String SAVED_NUM = "NUMBER";
+    static SharedPreferences sharedPreferences;
+
+    static void saveData(Context context) {
+        sharedPreferences = context.getSharedPreferences(SAVED_NUM, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(SAVED_NUM, Integer.parseInt(String.valueOf(maxScore)));
+        editor.apply();
+    }
+
+    static void loadData(Context context) {
+        sharedPreferences = context.getSharedPreferences(SAVED_NUM, MODE_PRIVATE);
+        maxScore = sharedPreferences.getInt(SAVED_NUM, 0);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gd.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    void right() {
+        boolean t = true;
+        for (int i = 0; i < shape[shapeNum][direction][0].length; i++) {
+            if (!GameView.rightCheck(mainBlockX + shape[shapeNum][direction][0][i],mainBlockY + shape[shapeNum][direction][1][i])) {
+                t = false;
+                break;
+            }
+        }
+        if (t)
+            mainBlockX++;
+    }
+
+    void left() {
+        boolean t = true;
+        for (int i = 0; i < shape[shapeNum][direction][0].length; i++) {
+            if (!GameView.leftCheck(mainBlockX + shape[shapeNum][direction][0][i],mainBlockY + shape[shapeNum][direction][1][i])) {
+                t = false;
+                break;
+            }
+        }
+        if (t)
+            mainBlockX--;
+    }
+
+    void up() {
+        direction++;
+        direction %= shape[shapeNum].length;
+    }
+
+    void down() {
+        timerInterval = 10;
+    }
+
+    //==============================================================================================
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        boolean result = false;
+        try {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        right();
+                    } else {
+                        left();
+                    }
+                    result = true;
+                }
+            } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    down();
+                } else {
+                    up();
+                }
+                result = true;
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+}
